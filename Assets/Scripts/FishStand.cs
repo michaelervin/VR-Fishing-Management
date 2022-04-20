6f665b2d7299ec4c0c94d5cc3a997547ac832f70
driedel152 <45524258+driedel152@users.noreply.github.com>
2022-04-20 17:55:58 -0500

    Added fish buying

--- Assets/Scripts/FishStand.cs
+++ Assets/Scripts/FishStand.cs
@@ -1,17 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class FishStand : MonoBehaviour
 {
+    [SerializeField] JerryBank bank;
     [SerializeField] MarketDisplay display;
     [SerializeField] FishSpawner spawner;
     [SerializeField] int availableCount;
     [SerializeField] float spacing;
     [SerializeField] GameObject pointer;
+    [SerializeField] GameObject buyButton;
+    [SerializeField] GameObject cantBuyText;
 
+    Hand.AttachmentFlags attachmentFlags = Hand.AttachmentFlags.ParentToHand | Hand.AttachmentFlags.TurnOnKinematic;
     List<Fish> availableFish = new List<Fish>();
     int index = 0;
+
+    Fish SelectedFish
+    {
+        get
+        {
+            try
+            {
+                return availableFish[index];
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                return null;
+            }
+        }
+    }
 
     private void Start()
     {
@@ -21,12 +41,46 @@ public class FishStand : MonoBehaviour
             fish.transform.position += -fish.transform.right * i * spacing;
             availableFish.Add(fish);
         }
-        display.SetDisplay(availableFish[index]);
+        RefreshDisplay();
     }
 
     private void Update()
     {
-        pointer.transform.position = availableFish[index].transform.position + Vector3.up / 2;
+        if (SelectedFish)
+        {
+            buyButton.SetActive(true);
+            pointer.SetActive(true);
+            pointer.transform.position = SelectedFish.transform.position + Vector3.up / 2;
+        }
+        else
+        {
+            buyButton.SetActive(false);
+            pointer.SetActive(false);
+        }
+    }
+
+    public void Buy(Hand hand)
+    {
+        double cost = (SelectedFish as IMarketable).BaseCost();
+        if (cost <= bank.jerryBucks)
+        {
+            bank.jerryBucks -= cost;
+            SelectedFish.transform.position = hand.transform.position;
+            hand.AttachObject(SelectedFish.gameObject, hand.GetBestGrabbingType(), attachmentFlags);
+            availableFish.Remove(SelectedFish);
+            if (index > 0) index--;
+            RefreshDisplay();
+        }
+        else
+        {
+            cantBuyText.SetActive(true);
+        }
+    }
+
+    private void RefreshDisplay()
+    {
+        display.SetDisplay(SelectedFish);
+        cantBuyText.SetActive(false);
     }
 
     public void NextIndex()
@@ -36,7 +90,7 @@ public class FishStand : MonoBehaviour
         {
             index = 0;
         }
-        display.SetDisplay(availableFish[index]);
+        RefreshDisplay();
     }
 
     public void PreviousIndex()
@@ -46,6 +100,6 @@ public class FishStand : MonoBehaviour
         {
             index = availableFish.Count - 1;
         }
-        display.SetDisplay(availableFish[index]);
+        RefreshDisplay();
     }
 }
--- Assets/Scripts/MarketDisplay.cs
+++ Assets/Scripts/MarketDisplay.cs
@@ -11,9 +11,18 @@ public class MarketDisplay : MonoBehaviour
 
     public void SetDisplay(IDisplayable displayable)
     {
-        DisplayInfo info = displayable.GetDisplayInfo();
-        displayName.text = info.title;
-        cost.text = $"${info.cost}";
-        image.sprite = info.sprite;
+        if (displayable != null)
+        {
+            DisplayInfo info = displayable.GetDisplayInfo();
+            displayName.text = info.title;
+            cost.text = $"${info.cost}";
+            image.sprite = info.sprite;
+        }
+        else
+        {
+            displayName.text = "No Fish";
+            cost.text = $"";
+            image.sprite = null;
+        }
     }
 }
