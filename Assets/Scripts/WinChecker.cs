b033ec3aa9f31362499288440604c8afd6db1622
driedel152 <45524258+driedel152@users.noreply.github.com>
2022-04-26 15:09:12 -0500

    Added sound effect to WinChecker

--- Assets/Scripts/WinChecker.cs
+++ Assets/Scripts/WinChecker.cs
@@ -9,15 +9,23 @@ public class WinChecker : MonoBehaviour
     [SerializeField] FishContainer container;
     [SerializeField] string[] requiredFish;
     [SerializeField] GameObject winText;
+    [SerializeField] AudioClip winSound;
 
     private void Start()
     {
         winText.SetActive(false);
         container.onAdd += CheckContainer;
+        container.onRemove += ResetDisplay;
+    }
+
+    private void ResetDisplay(Fish fish)
+    {
+        winText.SetActive(false);
     }
 
     private void CheckContainer(Fish obj)
     {
+        if (winText.activeInHierarchy) return;
         foreach (string fish in requiredFish)
         {
             if(!(obj.data.name == fish) && !(container.objects.Find(f => f.data.name == fish) != null))
@@ -27,5 +35,6 @@ public class WinChecker : MonoBehaviour
             }
         }
         winText.SetActive(true);
+        AudioSource.PlayClipAtPoint(winSound, winText.transform.position);
     }
 }
